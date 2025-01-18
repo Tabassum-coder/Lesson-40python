df383ccc255af5521a77d985182fefed2360f37f
Tabassum-coder <tabuj2706@gmail.com>
2025-01-18 23:39:11 +0530

    act1

--- acp.py
+++ acp.py
@@ -2,12 +2,13 @@
 
 sum=0
 num=int(input("Enter a number"))
+#153
 
 temp=num
 while temp>0:
-    digit=temp%10
-    sum=sum+digit**3
-    temp=temp//10
+    digit=temp%10 #153%10=3 #15%10=5     #1%10=1
+    sum=sum+digit**3#sum=0+27 #sum=27+125 sum=27+125+1=153
+    temp=temp//10#153//10=15  #15//10=1   #1//10=0
 
 if sum==num:
     print(num," is armstrong number")
--- activity1.py
+++ activity1.py
@@ -2,4 +2,4 @@
 
 num=int(input("Enter the number"))
 for i in range(1,11):
-    print(num," * {0} = {1}".format(i,(num*i)))
+    print(num," * ",i," = ",(num*i))
